63726d60351d0189044efb940f68b87323ce65e2
Kevin Cheung <kevinchikitcheung@gmail.com>
2019-04-30 17:19:53 -0500

    adjusted wallpaper css to match any browser

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     return (
       <Provider store={store}>
         <div className="App">
-          <Wallpaper/>
+          {/* <Wallpaper/> */}
         </div>
       </Provider>
     );
